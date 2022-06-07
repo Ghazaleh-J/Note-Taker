8db17620792cb75215acf54f893b640521a1be9e
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-06-07 12:32:50 -0700

    deploying & post route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+ // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 router.get('/notes', (req, res) => {
 
@@ -13,9 +15,19 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) =>{
   console.log(req.body)
+  let newNote = req.body
+  newNote.id = uuidv4();
+  fs.readFile("./db/db.json","utf8", (err,data)=>{
+    if (err){ console.log(err)}
+    let noteArray = JSON.parse(data)
+    noteArray.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(noteArray), (err) =>{
+      if (err){ console.log(err)}});
+
+    
+ })
+ res.json(newNote)
 });
-
-
 
 
 
